eb250a7a33f8b0ef454bd0d7e144b1f107992c4d
Alvanzlfkr <mohamadalvanzulfikhar@gmail.com>
2025-11-25 16:13:22 +0700

    first commit

---
+++ src/main/java/com/mycompany/pert3_50422896/Pert3_50422896.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pert3_50422896;
+
+/**
+ *
+ * @author Van
+ */
+public class Pert3_50422896 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
